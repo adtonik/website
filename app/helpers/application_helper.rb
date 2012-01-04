11af7140d4d1f3e32c9260e0f851d2c8b736bd93
Marshall A. Beddoe <unmarshal@gmail.com>
2012-01-03 20:00:09 -0800

    spotlights, radial light, logo, tv sans text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # generate CSS class names as controller-<controller> and action-<controller>-<action>
+  # for controller and method scoped CSS
+  def controller_method_css_class
+    "controller-#{controller.controller_path.sub('/', '_')} action-#{controller.controller_path.sub('/', '_')}-#{controller.action_name}"
+  end  
 end
