b317a56bcd2e70a1e2aa8b8fe8c0712c38f60d2e
Marshall A. Beddoe <unmarshal@gmail.com>
2012-01-07 21:07:16 -0800

    capture email addresses in database

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,14 @@
-class HomepageController < ApplicationController
+class HomepageController < ApplicationController  
   def index
+  end
+  
+  def email_submit    
+    if params['email_address']
+      Email.create(:email => params[:email_address], :ip_address => request.remote_ip)
+    end
+    
+    respond_to do |format|
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Website::Application.routes.draw do
   root :to => 'homepage#index'
+  
+  match '/email_submit', to: "homepage#email_submit", as: 'email_submit'
 end
