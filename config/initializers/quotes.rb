01c0279cfa692e307daae90ccd7513b7a47c172c
Marshall A. Beddoe <unmarshal@gmail.com>
2012-01-08 15:29:39 -0800

    fixing grammar in quotes

--- config/initializers/quotes.rb
+++ config/initializers/quotes.rb
@@ -6,8 +6,8 @@ QUOTES =
   "Think big, think global, and think beyond the TV box. -- Eric Schmidt",
   "History shows that in the face of new technology, those who adapt their business models don't just survive, they prosper. -- Eric Schmidt",
   "TV's only real competition was boredom and TV was winning. -- Mark Cuban",
-  "You Watch a lot of Web Video. You Watch Way More TV. -- Peter Kafka, WSJ All Things Digital Blog",
-  "The average 18-49 yr old watches 20 times more Traditional TV than web video and 12 times more Linear TV than Time Delayed TV. -- Nielsen",
+  "You watch a lot of web video. You watch way more TV. -- Peter Kafka, WSJ All Things Digital Blog",
+  "The average 18-49 yr old watches 20 times more traditional TV than web video and 12 times more linear TV than time delayed TV. -- Nielsen",
   "Did you know that almost half of the TV shows that are recorded are played back on the same day? -- ReadWriteWeb",
   "From what we're seeing, any media company that doesn't have a scalable advertiser funded proposition really doesn't have a sustainable business model. -- Antony Young, CEO Mindshare",
   "At the beginning of 2011 BitTorrent had 100 million users, more than Netflix and Hulu combined. -- Lost Remote"
