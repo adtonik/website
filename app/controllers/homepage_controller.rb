c02df82a4de7e905fd278dfeeb35f29d441bdb7a
Marshall A. Beddoe <unmarshal@gmail.com>
2012-01-08 14:49:45 -0800

    quotes with rotator

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,12 +1,19 @@
-class HomepageController < ApplicationController  
+class HomepageController < ApplicationController
   def index
+    
+    @quotes = QUOTES.map do |q|
+      quote, author = q.split("--")
+      { quote: quote.strip, author: author.strip }
+    end
+    
+    @left_quotes, @right_quotes = @quotes.each_slice(QUOTES.size / 2).to_a
   end
-  
-  def email_submit    
+
+  def email_submit
     if params['email_address']
       Email.create(:email => params[:email_address], :ip_address => request.remote_ip)
     end
-    
+
     respond_to do |format|
       format.js
     end
---
+++ config/initializers/quotes.rb
@@ -0,0 +1,14 @@
+QUOTES =
+[
+  "Marketers are demanding a better overall picture of who is actually watching their TV ads, where they are and when. -- MarketingWeek",
+  "TV viewing is high according to the latest statistics, which show that the average viewer watched more commercial television in 2011 than ever before. -- MarketingWeek",
+  "It has been alarming to see the gap in expectations between broadcasters and the advertisers that bankroll them. -- MarketingWeek",
+  "Think big, think global, and think beyond the TV box. -- Eric Schmidt",
+  "History shows that in the face of new technology, those who adapt their business models don't just survive, they prosper. -- Eric Schmidt",
+  "TV's only real competition was boredom and TV was winning. -- Mark Cuban",
+  "You Watch a lot of Web Video. You Watch Way More TV. -- Peter Kafka, WSJ All Things Digital Blog",
+  "The average 18-49 yr old watches 20 times more Traditional TV than web video and 12 times more Linear TV than Time Delayed TV -- Nielsen",
+  "Did you know that almost half of the TV shows that are recorded are played back on the same day? -- ReadWriteWeb",
+  "From what we're seeing, any media company that doesn't have a scalable advertiser funded proposition really doesn't have a sustainable business model -- Antony Young, CEO Mindshare",
+  "At the beginning of 2011 BitTorrent had 100 million users, more than Netflix and Hulu combined. -- Lost Remote"
+]
